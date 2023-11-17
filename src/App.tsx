8715c466c4ac5b87fe78547f8b6c476194066353
luzhaopan <745003249@qq.com>
2023-11-17 17:58:01 +0800

    fix: del

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 
-// import { Suspense } from 'react'
-// import { RouterProvider } from 'react-router-dom';
-// import Loading from '@/components/Loading'
-import RenderRouter from '@/router'
+import { Suspense } from 'react'
+import { BrowserRouter } from "react-router-dom";
 import { ConfigProvider } from "antd";
 // import enUS from "antd/es/locale/en_US";
 import zhCN from "antd/es/locale/zh_CN";
-import { BrowserRouter } from "react-router-dom";
+import Loading from '@/components/Loading'
+import RenderRouter from '@/router'
+
 import './App.less'
 // HashRouter和BrowserRouter决定了路由模式分别是hash模式和history模式，并且这两个组件是路由的容器，必须放在最外层。
 function App() {
@@ -18,13 +18,12 @@ function App() {
   //   }
   // };
   return (
-    // <Suspense fallback={<Loading />}>
-    //   <RouterProvider router={router}></RouterProvider>
-    // </Suspense>
     <ConfigProvider locale={zhCN} componentSize="middle">
-      <BrowserRouter>
-        <RenderRouter />
-      </BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <BrowserRouter>
+          <RenderRouter />
+        </BrowserRouter>
+      </Suspense>
     </ConfigProvider>
   )
 }
